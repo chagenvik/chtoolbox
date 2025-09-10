4a95a7f897dc1e7c6f921c033fce98770fbf217d
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2025-09-10 10:03:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with readme_path.open() as readme_file:
 
 setup(
     name='chtoolbox',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=requirements,
     long_description=long_description,
